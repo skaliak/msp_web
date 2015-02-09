9a7b2c8bcc2a555c7d8c37338b85953f2e97bdf1
skaliak <info@skaliak.com>
2015-02-08 21:53:24 -0800

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mspWebApp
+ * @description
+ * # mspWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mspWebApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
